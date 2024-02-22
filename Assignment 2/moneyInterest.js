b1ce5f00e630e3d04f7447e768bc2579794e65c4
Sreelekshmiust <127503506+Sreelekshmiust@users.noreply.github.com>
2024-02-22 12:03:43 +0530

    Create moneyInterest.js

---
+++ Assignment 2/moneyInterest.js
@@ -0,0 +1,21 @@
+export function calculateSI(principleamount, rate, time) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(principleamount) || isNaN(rate) || isNaN(time)) {
+      reject(new Error('Invalid input. Please enter valid numbers for principal amount, rate, and time.'));
+    } else {
+      const res = (principleamount * rate * time) / 100;
+      resolve(res);
+    }
+  });
+}
+ 
+ 
+export function calculateCI(principalamount,rate,time,compoundingsPerYear) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+    const finalAmount = principalamount * Math.pow((1 + rate / (100 * compoundingsPerYear)), compoundingsPerYear * time);
+    const compoundInterest = finalAmount - principalamount;
+    resolve(compoundInterest);
+        }, 5000);
+    });
+}
