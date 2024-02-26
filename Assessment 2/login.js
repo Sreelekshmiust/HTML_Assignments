76bba40f6c9c6ed205ce52fc4d9ee519dbcbd819
Sreelekshmiust <127503506+Sreelekshmiust@users.noreply.github.com>
2024-02-26 12:40:14 +0530

    Update login.js

--- Assessment 2/login.js
+++ Assessment 2/login.js
@@ -1,22 +1,34 @@
-        function loginUser() {
-        // getting the values from html document
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
+function checkFields() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var submitBtn = document.getElementById("submitBtn");
 
-            // Retrieving the stored user data from local storage
-            var userData = localStorage.getItem('user');
-            if (userData) {
-                // converting the json string into js object
-                var user = JSON.parse(userData);
-                if (user.username === username && user.password === password) {
-                    window.location.href = 'profile.html';
-                    return false; 
-                } else {
-                    alert("Invalid username or password");
-                    return false; 
-                }
-            } else {
-                alert("No user registered. Please sign up first.");
-                return false; 
-            }
+    if (username.trim() !== "" && password.trim() !== "") {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+}
+
+function loginUser() {
+    // getting the values from html document
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Retrieving the stored user data from local storage
+    var userData = localStorage.getItem('user');
+    if (userData) {
+        // converting the json string into js object
+        var user = JSON.parse(userData);
+        if (user.username === username && user.password === password) {
+            window.location.href = 'profile.html';
+            return false;
+        } else {
+            alert("Invalid username or password");
+            return false;
         }
+    } else {
+        alert("No user registered. Please sign up first.");
+        return false;
+    }
+}
